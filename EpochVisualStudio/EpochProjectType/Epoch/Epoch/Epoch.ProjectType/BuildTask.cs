bfa891bce46166927d983fc7ee36f3c2810011a1
Apoch <don.apoch@gmail.com>
2016-09-17 19:35:45 -0700

    Remove hardcoded output path for .EXEs

--- EpochVisualStudio/EpochProjectType/Epoch/Epoch/Epoch.ProjectType/BuildTask.cs
+++ EpochVisualStudio/EpochProjectType/Epoch/Epoch/Epoch.ProjectType/BuildTask.cs
@@ -12,12 +12,20 @@ namespace EpochVS
     public class BuildTask : Microsoft.Build.Utilities.Task
     {
         private string m_fileName;
+        private string m_outputName;
 
         [Required]
         public string Filename
         {
             get { return m_fileName; }
             set { m_fileName = value; }
+        }
+
+        [Required]
+        public string Output
+        {
+            get { return m_outputName; }
+            set { m_outputName = value; }
         }
 
         public override bool Execute()
@@ -27,7 +35,7 @@ namespace EpochVS
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "d:\\epoch\\epoch-language\\bin\\debug\\EpochNativeBin.exe",      // TODO - remove hardcoded paths
-                    Arguments = "/files " + m_fileName + " /output D:\\Epoch\\Scratch\\VSBuilds\\EpochProgram.exe",
+                    Arguments = "/files " + m_fileName + " /output " + m_outputName,
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
                     CreateNoWindow = true
