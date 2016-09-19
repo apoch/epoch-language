bd6d6b60fc3935fe26cab2b127dc908816148f32
Apoch <don.apoch@gmail.com>
2016-09-18 18:08:45 -0700

    Fix build-up-to-date checking

--- EpochVisualStudio/EpochProjectType/Epoch/Epoch/Epoch.ProjectType/BuildUpToDateCheckProvider.cs
+++ EpochVisualStudio/EpochProjectType/Epoch/Epoch/Epoch.ProjectType/BuildUpToDateCheckProvider.cs
@@ -9,26 +9,26 @@ using Microsoft.VisualStudio.Threading;
 
 namespace EpochVS
 {
-    [Export(typeof(IBuildUpToDateCheckProvider))]
-    [AppliesTo(MyUnconfiguredProject.UniqueCapability)]
-    internal class BuildUpToDateCheckProvider : IBuildUpToDateCheckProvider
-    {
-        public async Task<bool> IsUpToDateAsync(BuildAction buildAction, TextWriter logger, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            // Since this method is likely to be I/O intensive, get off the UI thread explicitly first.
-            await TaskScheduler.Default;
+    //[Export(typeof(IBuildUpToDateCheckProvider))]
+    //[AppliesTo(MyUnconfiguredProject.UniqueCapability)]
+    //internal class BuildUpToDateCheckProvider : IBuildUpToDateCheckProvider
+    //{
+    //    public async Task<bool> IsUpToDateAsync(BuildAction buildAction, TextWriter logger, CancellationToken cancellationToken = default(CancellationToken))
+    //    {
+    //        // Since this method is likely to be I/O intensive, get off the UI thread explicitly first.
+    //        await TaskScheduler.Default;
 
-            // TODO: Return true when the project is up-to-date to skip invoking the build.
+    //        // TODO: Return true when the project is up-to-date to skip invoking the build.
 
-            // It's better to err on the side of caution by building without needing to than not building when we should have.
-            return false;
-        }
+    //        // It's better to err on the side of caution by building without needing to than not building when we should have.
+    //        return false;
+    //    }
 
-        public Task<bool> IsUpToDateCheckEnabledAsync(CancellationToken cancellationToken = default(CancellationToken))
-        {
-            // If you implement this method and you want to be async, add the async keyword to the method signature
-            // and change the return statement to "return true;" instead of calling Task.FromResult.
-            return Task.FromResult(true);
-        }
-    }
+    //    public Task<bool> IsUpToDateCheckEnabledAsync(CancellationToken cancellationToken = default(CancellationToken))
+    //    {
+    //        // If you implement this method and you want to be async, add the async keyword to the method signature
+    //        // and change the return statement to "return true;" instead of calling Task.FromResult.
+    //        return Task.FromResult(true);
+    //    }
+    //}
 }
