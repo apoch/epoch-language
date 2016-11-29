5d43231d4acfda3076fabbb95a73947ffa0a748f
Apoch <don.apoch@gmail.com>
2016-11-28 21:22:05 -0800

    Rebuild infrastructure for compiling and debugging in the VSIX

---
+++ EpochVisualStudio/EpochVSIX/EpochVSIX/EpochVSIX.ProjectType/DebuggerLaunchProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.Debuggers;
+using Microsoft.VisualStudio.ProjectSystem.Utilities;
+using Microsoft.VisualStudio.ProjectSystem.Utilities.DebuggerProviders;
+using Microsoft.VisualStudio.ProjectSystem.VS.Debuggers;
+
+namespace EpochVSIX
+{
+    [ExportDebugger(Debugger.SchemaName)]
+    [AppliesTo(MyUnconfiguredProject.UniqueCapability)]
+    public class DebuggerLaunchProvider : DebugLaunchProviderBase
+    {
+        [ExportPropertyXamlRuleDefinition("EpochVSIX, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9be6e469bc4921f1", "XamlRuleToCode:Debugger.xaml", "Project")]
+        [AppliesTo(MyUnconfiguredProject.UniqueCapability)]
+        private object DebuggerXaml { get { throw new NotImplementedException(); } }
+
+        [ImportingConstructor]
+        public DebuggerLaunchProvider(ConfiguredProject configuredProject)
+            : base(configuredProject)
+        {
+        }
+
+        /// <summary>
+        /// Gets project properties that the debugger needs to launch.
+        /// </summary>
+        [Import]
+        private ProjectProperties DebuggerProperties { get; set; }
+
+        public override async Task<bool> CanLaunchAsync(DebugLaunchOptions launchOptions)
+        {
+            var properties = await this.DebuggerProperties.GetDebuggerPropertiesAsync();
+            string commandValue = await properties.DebuggerCommand.GetEvaluatedValueAtEndAsync();
+            return !string.IsNullOrEmpty(commandValue);
+        }
+
+        public override async Task<IReadOnlyList<IDebugLaunchSettings>> QueryDebugTargetsAsync(DebugLaunchOptions launchOptions)
+        {
+            var settings = new DebugLaunchSettings(launchOptions);
+
+            // The properties that are available via DebuggerProperties are determined by the property XAML files in your project.
+            var debuggerProperties = await this.DebuggerProperties.GetDebuggerPropertiesAsync();
+            settings.CurrentDirectory = await debuggerProperties.DebuggerWorkingDirectory.GetEvaluatedValueAtEndAsync();
+            settings.Executable = await debuggerProperties.DebuggerCommand.GetEvaluatedValueAtEndAsync();
+            settings.Arguments = await debuggerProperties.DebuggerCommandArguments.GetEvaluatedValueAtEndAsync();
+            settings.LaunchOperation = DebugLaunchOperation.CreateProcess;
+
+            settings.LaunchDebugEngineGuid = DebuggerEngines.NativeOnlyEngine;
+
+            return new IDebugLaunchSettings[] { settings };
+        }
+    }
+}
---
+++ EpochVisualStudio/EpochVSIX/EpochVSIX/EpochVSIX.ProjectType/PropertyProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.Utilities;
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EpochVSIX
+{
+    [Export(typeof(IProjectGlobalPropertiesProvider))]
+    [AppliesTo(MyUnconfiguredProject.UniqueCapability)]
+    public class PropertyProvider : StaticGlobalPropertiesProviderBase
+    {
+        [ImportingConstructor]
+        public PropertyProvider(IThreadHandling handling)
+            : base(handling.JoinableTaskContext)
+        {
+        }
+
+        public override Task<IImmutableDictionary<string, string>> GetGlobalPropertiesAsync(CancellationToken cancellationToken)
+        {
+            string dllpath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            IImmutableDictionary<string, string> properties = Empty.PropertiesMap
+                .SetItem("EpochVSExtensionPath", dllpath);
+
+            return Task.FromResult<IImmutableDictionary<string, string>>(properties);
+        }
+    }
+}
