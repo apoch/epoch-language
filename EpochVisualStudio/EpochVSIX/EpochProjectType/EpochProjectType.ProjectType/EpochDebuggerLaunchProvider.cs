297f82ab74373f4d9d3ced6307a538adfb6bd931
Mike Lewis <don.apoch@gmail.com>
2018-04-03 20:48:21 -0700

    Redo the debugger integration, because redoing things for no reason is the flavor of the week

--- EpochVisualStudio/EpochVSIX/EpochProjectType/EpochProjectType.ProjectType/EpochDebuggerLaunchProvider.cs
+++ EpochVisualStudio/EpochVSIX/EpochProjectType/EpochProjectType.ProjectType/EpochDebuggerLaunchProvider.cs
@@ -9,17 +9,18 @@ using Microsoft.VisualStudio.ProjectSystem.VS.Debug;
 
 namespace EpochVSIX
 {
-    [ExportDebugger(Debugger.SchemaName)]
+    [ExportDebugger(EpochDebugger.SchemaName)]
     [AppliesTo(MyUnconfiguredProject.UniqueCapability)]
-    public class DebuggerLaunchProvider : DebugLaunchProviderBase
+    public class EpochDebuggerLaunchProvider : DebugLaunchProviderBase
     {
         [ImportingConstructor]
-        public DebuggerLaunchProvider(ConfiguredProject configuredProject)
+        public EpochDebuggerLaunchProvider(ConfiguredProject configuredProject)
             : base(configuredProject)
         {
         }
 
-        [ExportPropertyXamlRuleDefinition("EpochVSIX, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9be6e469bc4921f1", "XamlRuleToCode:Debugger.xaml", "Project")]
+        // TODO: Specify the assembly full name here
+        [ExportPropertyXamlRuleDefinition("EpochVSIX, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9be6e469bc4921f1", "XamlRuleToCode:EpochDebugger.xaml", "Project")]
         [AppliesTo(MyUnconfiguredProject.UniqueCapability)]
         private object DebuggerXaml { get { throw new NotImplementedException(); } }
 
@@ -31,8 +32,8 @@ namespace EpochVSIX
 
         public override async Task<bool> CanLaunchAsync(DebugLaunchOptions launchOptions)
         {
-            var properties = await this.DebuggerProperties.GetDebuggerPropertiesAsync();
-            string commandValue = await properties.DebuggerCommand.GetEvaluatedValueAtEndAsync();
+            var properties = await this.DebuggerProperties.GetEpochDebuggerPropertiesAsync();
+            string commandValue = await properties.EpochDebuggerCommand.GetEvaluatedValueAtEndAsync();
             return !string.IsNullOrEmpty(commandValue);
         }
 
@@ -41,10 +42,10 @@ namespace EpochVSIX
             var settings = new DebugLaunchSettings(launchOptions);
 
             // The properties that are available via DebuggerProperties are determined by the property XAML files in your project.
-            var debuggerProperties = await this.DebuggerProperties.GetDebuggerPropertiesAsync();
-            settings.CurrentDirectory = await debuggerProperties.DebuggerWorkingDirectory.GetEvaluatedValueAtEndAsync();
-            settings.Executable = await debuggerProperties.DebuggerCommand.GetEvaluatedValueAtEndAsync();
-            settings.Arguments = await debuggerProperties.DebuggerCommandArguments.GetEvaluatedValueAtEndAsync();
+            var debuggerProperties = await this.DebuggerProperties.GetEpochDebuggerPropertiesAsync();
+            settings.CurrentDirectory = await debuggerProperties.EpochDebuggerWorkingDirectory.GetEvaluatedValueAtEndAsync();
+            settings.Executable = await debuggerProperties.EpochDebuggerCommand.GetEvaluatedValueAtEndAsync();
+            settings.Arguments = await debuggerProperties.EpochDebuggerCommandArguments.GetEvaluatedValueAtEndAsync();
             settings.LaunchOperation = DebugLaunchOperation.CreateProcess;
 
             settings.LaunchDebugEngineGuid = DebuggerEngines.NativeOnlyEngine;
