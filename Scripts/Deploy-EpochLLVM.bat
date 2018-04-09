485c02fdeb3c7dd09af1f03021a09a875430b8ac
Mike Lewis <don.apoch@gmail.com>
2018-04-08 20:14:26 -0700

    Make some tiny improvements to PDB generation hacks

--- Scripts/Deploy-EpochLLVM.bat
+++ Scripts/Deploy-EpochLLVM.bat
@@ -1,2 +1,4 @@
 @echo off
 copy /y "D:\Epoch\epoch-language\EpochDevTools32\bin\Debug\EpochLLVM.dll" "C:\Code\epoch-language\EpochDevTools32\Debug\x86"
+copy /y "D:\Epoch\epoch-language\EpochDevTools32\bin\Debug\EpochLLVM.pdb" "C:\Code\epoch-language\EpochDevTools32\Debug\x86"
+
