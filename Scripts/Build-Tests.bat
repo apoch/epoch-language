db1d1e1f8f173a0568dbc43b338ba2bf006a5518
Mike Lewis <don.apoch@gmail.com>
2018-05-20 18:21:17 -0700

    Add some test cases for pinpointing a rogue shallow copy bug

---
+++ Scripts/Build-Tests.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Code\epoch-language\EpochDevTools32\Debug\x86\Compiler32.exe" /files "C:\Code\epoch-language\EpochTests\Scratch\TestBootstrapping.epoch" /output "C:\Code\epoch-language\EpochTests\Scratch\Bin\TestBootstrapping.exe"
