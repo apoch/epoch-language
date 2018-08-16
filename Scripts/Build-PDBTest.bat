4a78fb758dac91c12a22eec66e5950a7f342c113
Mike Lewis <don.apoch@gmail.com>
2018-08-16 06:22:28 -0700

    Capture output of PDBTest builds to disk

--- Scripts/Build-PDBTest.bat
+++ Scripts/Build-PDBTest.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Code\epoch-language\EpochDevTools32\Debug\x86\Compiler32.exe" /files "C:\Code\epoch-language\EpochTests\PDBTest\EntryPoint.epoch" /output "C:\Code\epoch-language\EpochTests\PDBTest\Debug\x64\PDBTest.exe"
+"C:\Code\epoch-language\EpochDevTools32\Debug\x86\Compiler32.exe" /files "C:\Code\epoch-language\EpochTests\PDBTest\EntryPoint.epoch" /output "C:\Code\epoch-language\EpochTests\PDBTest\Debug\x64\PDBTest.exe" 2>"C:\Code\epoch-language\EpochTests\PDBTest\Debug\x64\pdbtestllvm.txt"
